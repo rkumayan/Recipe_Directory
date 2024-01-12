833101ac2a2b4df94d603c4b54753cfe43ca8785
rkumayan <kumayanrahul01@gmail.com>
2024-01-12 22:19:49 +0530

    fetched the data from backend

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,20 @@
 import './Home.css'
+import {useFetch} from '../../hooks/useFetch'
 const Home = () => {
+    const {data, isPending, error} = useFetch("http://localhost:3000/recipes");
     return (  
-        <div>
-            Home
+        <div className='Home'>
+            { error && <p className='error'> {error} </p> }
+            {isPending && <p className="loading">Loading...</p> }
+            {   data && 
+                data.map( recipe =>(
+                    <div className="recipe">
+                        <h2>{recipe.title}</h2>
+                        <p> {recipe.method}</p>
+                        <p> {recipe.cookingTime} </p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
