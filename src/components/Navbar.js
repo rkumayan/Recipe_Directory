204d0063a0f9fa20ae24d075846822b8e988aa62
rkumayan <kumayanrahul01@gmail.com>
2024-01-12 22:08:53 +0530

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <nav>
+                <Link to = "/" className='brand'>
+                    <h1>Cooking Komi</h1>
+                </Link>
+                <Link to = "/create"> Create Recipe </Link>
+            </nav>
+            
+        </div>
+     );
+}
+ 
+export default Navbar;
