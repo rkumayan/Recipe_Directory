60e56b514065fef7c59c9150d56f130a7a45286d
rkumayan <kumayanrahul01@gmail.com>
2024-01-13 15:48:16 +0530

    added recipe component for viewing individual components

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,9 +1,26 @@
 import './Recipe.css'
-
+import useFetch from "../../hooks/useFetch"
+import { useParams } from 'react-router-dom'
 const Recipe = () => {
+    const id = useParams().id;
+    const {data: recipe, isPending, error} = useFetch( "http://localhost:3000/recipes/" + id);
+
     return ( 
-        <div>
-            Recipe page
+        <div className='recipe'>
+            {error && <p className='error' > {error} </p> }
+            {isPending && <p> loading...</p>}
+            {recipe && (
+                <>
+                    <h3 className="recipe title"> {recipe.title} </h3>
+                    <p> {recipe.cookingTime} to cook</p>
+                    <ul>
+                        { recipe.ingredients.map( ing =>(
+                            <li key = {ing}> {ing}</li>
+                        ))}
+                    </ul>
+                    <p className="method">{ recipe.method} </p>
+                </>
+            )}
         </div>
     );
 }
