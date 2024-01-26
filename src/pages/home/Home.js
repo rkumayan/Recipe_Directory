a630ec2005b74628826bf7ff55430e3aa6526b06
rkumayan <kumayanrahul01@gmail.com>
2024-01-26 12:26:55 +0530

    aded feature for fetching data from firebase in home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,9 +2,39 @@ import './Home.css'
 import {useFetch} from '../../hooks/useFetch'
 import RecipeList from '../../components/RecipeList';
 import { useTheme} from '../../hooks/useTheme';
+
+import { projectFirestore } from '../../firebase/config';
+import { useEffect, useState} from 'react';
 const Home = () => {
-    const {recipeUrl}= useTheme();
-    const {data, isPending, error} = useFetch(recipeUrl);
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
+  
+    useEffect( () =>{
+        setIsPending(true)
+        projectFirestore.collection('recipes').get()
+            .then( (snapshot)=>{
+                console.log(snapshot);
+                if(snapshot.empty){
+                    setError(' No recipes to load')
+                    setIsPending(false);
+                }
+                else{
+                    let results = [];
+                    snapshot.docs.forEach( doc =>{
+                        results.push({ id:doc.id, ...doc.data()})
+                    })
+                    setData(results);
+                    setIsPending(false);
+                }
+            })
+            .catch( err =>{
+                setError(err.message);
+                setIsPending(false);
+            })
+    } , []);
+
+    
     return (  
         <div className='Home'>
             { error && <p className='error'> {error} </p> }
