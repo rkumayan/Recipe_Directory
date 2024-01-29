05e824b69f140589974bd7889617a3485ac35c07
rkumayan <kumayanrahul01@gmail.com>
2024-01-29 18:08:26 +0530

    added functionality to fetch single document from firebase in Recipe component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -2,11 +2,31 @@ import './Recipe.css'
 import useFetch from "../../hooks/useFetch"
 import { useParams } from 'react-router-dom'
 import {useTheme} from '../../hooks/useTheme'
+import { useEffect,useState } from 'react'
+import { projectFirestore } from '../../firebase/config'
 const Recipe = () => {
     const id = useParams().id;
     
-    const {mode, recipeUrl} = useTheme();
-    const {data: recipe, isPending, error} = useFetch( recipeUrl + id);
+    const {mode} = useTheme();
+    // const {data: recipe, isPending, error} = useFetch( recipeUrl + id);
+    const [recipe, setRecipe] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect( ()=>{
+        setIsPending(true);
+        projectFirestore.collection('recipes').doc(id).get()
+            .then( doc=>{
+                if( doc.exists){
+                    setIsPending(false);
+                    setRecipe( doc.data());
+                }
+                else{
+                    setIsPending(false);
+                    setError('Could not fetch the recipe')
+                }
+            })
+    }, [id])
     return ( 
         <div className= {`recipe ${mode}`}>
             {error && <p className='error' > {error} </p> }
