ab7173a0e4163711fcfc834523e77b24a6dea2bf
rkumayan <kumayanrahul01@gmail.com>
2024-01-30 17:40:37 +0530

    added functionality to delete single recipe

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,9 +1,19 @@
 import './RecipeList.css'
 import {Link} from 'react-router-dom'
 import {useTheme} from '../hooks/useTheme';
+import deleteIcon from '../assets/deleteIcon.svg'
+import {projectFirestore} from '../firebase/config';
+
 const RecipeList = ( {recipes}) => {
     const {mode} = useTheme();
 
+    const handleDelete = (id)=>{
+        projectFirestore.collection('recipes').doc(id).delete()
+            .then( () => {
+                window.location.reload(false);
+            })
+        
+    }
     return ( 
         <div className= {`recipe-list`}>
             { recipes.map( recipe => (
@@ -13,6 +23,10 @@ const RecipeList = ( {recipes}) => {
 
                     <div> {recipe.method.substring(0,100)}...</div>
                     <Link to = {`/recipe/${recipe.id}`}> Cook This</Link>
+                    <img className='delete'
+                        src= {deleteIcon}
+                        onClick={ () => handleDelete(recipe.id)}
+                    />
                 </div>
             ))}
         </div>
